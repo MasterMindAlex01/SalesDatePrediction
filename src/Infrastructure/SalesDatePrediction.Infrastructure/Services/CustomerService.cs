eb1ddcbdb4802c02e4570c789b8be9eead6d0558
MasterMindAlex01 <30800345+MasterMindAlex01@users.noreply.github.com>
2024-09-07 08:19:11 -0500

    Ajustes

--- src/Infrastructure/SalesDatePrediction.Infrastructure/Services/CustomerService.cs
+++ src/Infrastructure/SalesDatePrediction.Infrastructure/Services/CustomerService.cs
@@ -29,15 +29,17 @@ namespace SalesDatePrediction.Infrastructure.Services
             }
 
             var paginatedResult = await query
-                .Select(x => x.Orders.Select(x => x.Orderid).Count() > 0 ? new CustomerResponse(x.Orders)
-                {
-                    Custid = x.Custid,
-                    CustomerName = x.Companyname,
-                }: new CustomerResponse(new List<Order>())
-                {
-                    Custid = x.Custid,
-                    CustomerName = x.Companyname,
-                }).AsNoTracking().ToPaginatedListAsync(filterRequest.PageNumber, filterRequest.PageSize);
+                .Select(x => x.Orders.Select(x => x.Orderid).Count() > 0 
+                    ? new CustomerResponse(x.Orders)
+                    {
+                        Custid = x.Custid,
+                        CustomerName = x.Companyname,
+                    }: new CustomerResponse(new List<Order>())
+                    {
+                        Custid = x.Custid,
+                        CustomerName = x.Companyname,
+                    }).AsNoTracking()
+                .ToPaginatedListAsync(filterRequest.PageNumber, filterRequest.PageSize);
             
             return paginatedResult;
         }
